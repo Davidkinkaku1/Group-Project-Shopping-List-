ef6a2b1b0294903d2057936d245bf2b1cc400b78
David Kinkaku <davidkinkaku1@gmail.com>
2021-09-02 13:09:09 -0500

    cart content

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,11 +3,8 @@ const cartContents = (props) => {
 return (
     <>
     <h2>Cart Contents</h2>
-    
-
-
-
-
+    <button type="submit">Reset</button>
+    <button type="submit" >clear</button>
     </>
     )
 }
