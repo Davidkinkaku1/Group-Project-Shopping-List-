eeb5c1343966e76fcb5e579a9b060c31cfe50748
David Kinkaku <davidkinkaku1@gmail.com>
2021-09-02 11:08:37 -0500

    adding a database

--- database.sql
+++ database.sql
@@ -1,2 +1,22 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+CREATE TABLE groceries(
+	"id" SERIAL PRIMARY KEY NOT NULL,
+	"item" VARCHAR(80) NOT NULL,
+	"quantity" DECIMAL(15,2),
+    "unit" VARCHAR(50) NOT NULL
+);
+
+INSERT INTO "groceries" 
+	("item", "quantity", "unit") 
+VALUES 
+	('Appels',3, '2 lbs'),
+	('Milk', 4, '1 gallon');
+	
+	--select all groceries
+SELECT * FROM "groceries";
+
+DROP TABLE "groceries";
+
