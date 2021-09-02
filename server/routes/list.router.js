d6dd1c13be463a6658b10e21bad61d01b59f0d20
Hunter Webster <huntertwebster@gmail.com>
2021-09-02 13:22:38 -0500

    post done

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,9 +4,8 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
-// GET
 
-//Gettting all the groceries from the database
+//GET: Gettting all the groceries from the database
 router.get('/', (req, res) => {
     // get ordered by ascending items
     const sqlText = `SELECT * FROM groceries ORDER BY item ASC;`;
@@ -22,7 +21,21 @@ router.get('/', (req, res) => {
 })
 
 
-
+// POST: posting for adding a new item to the database
+router.post('/', (req, res) => {
+    const grocery = req.body;
+    const sqlText = `INSERT INTO groceries ("item", "quantity", "unit")
+                     VALUES ($1, $2, $3)`;
+    pool.query(sqlText, [grocery.item, grocery.quantity, grocery.unit])
+        .then((result) => {
+            console.log(`Added some grocery data to the database`, guest);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+})
 
 
 module.exports = router;
