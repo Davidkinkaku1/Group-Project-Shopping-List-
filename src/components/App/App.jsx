146174499af1a713aa2d23d98538e269f27a6284
Hunter Webster <huntertwebster@gmail.com>
2021-09-02 16:28:43 -0500

    cart fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,71 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-import Header from '../Header/Header.jsx'
-import InputsForm from '../Inputs/inputs.jsx';  
-import cartContents from '../Cart/Cart.jsx';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Header from "../Header/Header.jsx";
+import InputsForm from "../Inputs/inputs.jsx";
+import cartContents from "../Cart/Cart.jsx";
 
-    let [grocieriesList, setgrocieriesList] = useState([]);
-    let [newItem, setnewItem] = useState(['']);
-    let [newQuantity, setnewQuantitiy] = useState([0]);
-    let [newUnit, setnewUnit] = useState(['']);
-    let [newPurchase, setNewPurchase] = useState([false]);
-
-
+let [grocieriesList, setgrocieriesList] = useState([]);
+let [newItem, setnewItem] = useState([""]);
+let [newQuantity, setnewQuantitiy] = useState([0]);
+let [newUnit, setnewUnit] = useState([""]);
+let [newPurchase, setNewPurchase] = useState([false]);
 
 function App() {
-    return (
-        <div className="App">
-            < Header />
-            < InputsForm />
-            < cartContents/>
-        </div>
-    );
-    // on load, get groceries
-    useEffect(() => {
-        getGroceries()
-    }, [])
+  // on load, get groceries
+  useEffect(() => {
+    getGroceries();
+  }, []);
 
-
-    const getGroceries = () => {
-        axios.get('/groceries')
-            .then(response => {
-            setGuestList(response.data)
-            })
-            .catch(err => {
-                alert('error getting groceries');
-                console.log(err);
-        })
-    }
-
-
-const addGrocery = () => {
-    axios.post('/groceries', { item: newItem, quantity: newQuantity, unit: newUnit, purchase: newPurchase})
-      .then(response => {
-        // clear inputs
-        setnewItem('');
-        setnewQuantitiy(0);
-        setnewUnit('');
-        setNewPurchase(false);
-        
-        
-          //call getGroceries
-        getGroceries();
+  const getGroceries = () => {
+    axios
+      .get("/groceries")
+      .then((response) => {
+        setGuestList(response.data);
       })
-      .catch(err => {
-        alert('Error Adding Grocery');
+      .catch((err) => {
+        alert("error getting groceries");
         console.log(err);
-      })
+      });
   };
 
-    return (
-        <div className="App">
-            < Header />
-            < InputsForm />
-            < cartContents/>
-        </div>
-    );
+  const addGrocery = () => {
+    axios
+      .post("/groceries", {
+        item: newItem,
+        quantity: newQuantity,
+        unit: newUnit,
+        purchase: newPurchase,
+      })
+      .then((response) => {
+        // clear inputs
+        setnewItem("");
+        setnewQuantitiy(0);
+        setnewUnit("");
+        setNewPurchase(false);
 
+        //call getGroceries
+        getGroceries();
+      })
+      .catch((err) => {
+        alert("Error Adding Grocery");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <InputsForm />
+      <cartContents />
+    </div>
+  );
 }
 
 export default App;
